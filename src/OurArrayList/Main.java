e6b331c17306b415879b5ac3296bc086c097a71b
melody <thgilnoomelody@gmail.com>
2020-07-15 17:54:24 +0900

    Add remove function and test code

--- src/OurArrayList/Main.java
+++ src/OurArrayList/Main.java
@@ -1,15 +1,26 @@
 package OurArrayList;
 
-import java.util.ArrayList;
-
 public class Main {
 	public static void main(String[] args) {
 		OurArrayList arr = new OurArrayList();
 		arr.add(1);
 		arr.add(2);
 		arr.add(3);
-		arr.print();
 		arr.add(1,9);
-		arr.print();
+		System.out.println(arr);
+		
+		arr.remove(0);
+		System.out.println(arr);
+		
+		arr.remove(2);
+		System.out.println(arr);
+		
+		arr.add(7);
+		arr.add(8);
+		arr.add(7);
+		System.out.println(arr);
+		
+		arr.remove(2);
+		System.out.println(arr);
 	}
 }
