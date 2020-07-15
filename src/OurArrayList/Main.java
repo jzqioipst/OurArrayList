0bbf8ae79085344c4be3b345f858726ebe2776da
kimmunhee <ansgml9884@naver.com>
2020-07-15 16:45:17 +0900

    add add(int e) function

--- src/OurArrayList/Main.java
+++ src/OurArrayList/Main.java
@@ -1,7 +1,9 @@
 package OurArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
 	}
 }
--- src/OurArrayList/OurArrayList.java
+++ src/OurArrayList/OurArrayList.java
@@ -8,11 +8,25 @@ public class OurArrayList {
 		//공간있으면 그냥 끝에 추가
 		//부족하면 array를 new int[더크게] 할당 받아서
 		//기존의 값들 모두 이사 + 새로운 값 추가
-		return false;
+		if(count==array.length) {
+			int [] newArray = new int[count*2];
+			for(int i = 0; i<array.length; i++) {
+				newArray[i] = array[i];
+			}
+			newArray[count++] = e;
+			array = newArray;
+		}else {
+			array[count++] =  e;
+		}
+		return true;
 	}
 	
 	public boolean add(int index, int e) {
 		//원하는 index에 요소 삽입
+		//원하는 index가 array의 사이즈보다 클 경우 false return
+		//새로 하나를 추가했을 때 배열의 크기가 지정된 크기보다 커질 경우
+		//
+		//현재 주어진 배열 사이즈로 충분히 담을 수 있는 경우
 		return false;
 	}
 	
