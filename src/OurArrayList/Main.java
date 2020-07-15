cb17cebb9892388fba92615004ed3912cb3aa759
kimmunhee <ansgml9884@naver.com>
2020-07-15 17:43:21 +0900

    edit add functions

--- src/OurArrayList/Main.java
+++ src/OurArrayList/Main.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		OurArrayList arr = new OurArrayList();
-		arr.add(1);
-		arr.add(2);
-		arr.add(3);
-		arr.print();
-		arr.add(1,9);
-		arr.print();
+
 	}
 }
